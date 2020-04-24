90c2c8868169388cb89de009db3c061fb0e2dc91
Nhi Kieu H <nhi.kieu@asiantech.vn>
2020-04-24 13:06:49 +0700

    Init Project, Setup cocoapod

---
+++ FinalProject/FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by MBA0176 on 4/24/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
