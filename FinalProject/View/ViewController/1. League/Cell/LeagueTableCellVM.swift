2eecc6ada52b62b8f509fefc095cf406bd4728c2
QuanBuiThien <quan.bui@asiantech.vn>
2020-04-27 23:06:53 +0700

    create ui league tablecell

---
+++ FinalProject/View/ViewController/1. League/Cell/LeagueTableCellVM.swift
@@ -0,0 +1,13 @@
+//
+//  LeagueTableCellVM.swift
+//  FinalProject
+//
+//  Created by Sếp Quân on 4/27/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import Foundation
+
+final class LeagueTableCellVM {
+    
+}
