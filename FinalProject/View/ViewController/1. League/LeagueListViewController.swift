f57d3a4081bea869d829c10c341b66c718c2e5d0
QuanBuiThien <quan.bui@asiantech.vn>
2020-04-27 18:42:01 +0700

    Commit create ui league: create SegmentedControl, custom Button, add tableView

--- FinalProject/View/ViewController/1. League/LeagueListViewController.swift
+++ FinalProject/View/ViewController/1. League/LeagueListViewController.swift
@@ -8,22 +8,59 @@
 
 import UIKit
 
-class LeagueListViewController: ViewController {
-
+final class LeagueListViewController: ViewController {
+    // MARK: - IBOutlet
+    @IBOutlet private weak var sportSegmentedControl: UISegmentedControl!
+    @IBOutlet private var nationsButton: [UIButton]!
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        for item in nationsButton {
+            item.layer.cornerRadius = item.bounds.width / 2
+            item.clipsToBounds = true
+        }
     }
-    */
-
+    
+    override func setupUI() {
+        super.setupUI()
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        sportSegmentedControl.setTitleTextAttributes(titleTextAttributes, for: .normal)
+        nationsButton[0].borderWidth = 3
+        nationsButton[0].borderColor = App.Color.greenCustomColor
+    }
+    
+    // MARK: - IBAction
+    @IBAction private func nationsButtonTouchUpInside(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            nationsButton.forEach { item in
+                item.borderWidth = 0
+            }
+            nationsButton[sender.tag].borderWidth = 3
+            nationsButton[sender.tag].borderColor = App.Color.greenCustomColor
+        case 1:
+            nationsButton.forEach { item in
+                item.borderWidth = 0
+            }
+            nationsButton[sender.tag].borderWidth = 3
+            nationsButton[sender.tag].borderColor = App.Color.greenCustomColor
+        case 2:
+            nationsButton.forEach { item in
+                item.borderWidth = 0
+            }
+            nationsButton[sender.tag].borderWidth = 3
+            nationsButton[sender.tag].borderColor = App.Color.greenCustomColor
+        default:
+            nationsButton.forEach { item in
+                item.borderWidth = 0
+            }
+            nationsButton[sender.tag].borderWidth = 3
+            nationsButton[sender.tag].borderColor = App.Color.greenCustomColor
+        }
+    }
 }
