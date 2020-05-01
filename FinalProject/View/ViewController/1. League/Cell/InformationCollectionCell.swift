be2dec3ac5363543d237267922be96ef7dd083be
QuanBuiThien <quan.bui@asiantech.vn>
2020-05-01 22:02:55 +0700

    Commit ui information cell, ui teams cell, ui photos cell, ui teams header

---
+++ FinalProject/View/ViewController/1. League/Cell/InformationCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  InformationCollectionCell.swift
+//  FinalProject
+//
+//  Created by Sếp Quân on 5/1/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import UIKit
+
+class InformationCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
