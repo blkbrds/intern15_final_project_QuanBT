87945e9f58799cf6c0e8bd4b2df2a13412475a36
QuanBuiThien <quan.bui@asiantech.vn>
2020-05-04 16:15:52 +0700

    create and ui DetailTeamHeaderView

---
+++ FinalProject/View/ViewController/1. League/Cell/DetailTeamHeaderVM.swift
@@ -0,0 +1,13 @@
+//
+//  DetailTeamHeaderVM.swift
+//  FinalProject
+//
+//  Created by Sếp Quân on 5/4/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import Foundation
+
+final class DetailTeamHeaderVM {
+    
+}
