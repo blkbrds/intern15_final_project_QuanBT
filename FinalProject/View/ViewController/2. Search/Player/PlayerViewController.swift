9875569e09558c96ef23883930ceb4e78e89e7cd
QuanBuiThien <quan.bui@asiantech.vn>
2020-05-08 12:01:34 +0700

    Commit ui player

---
+++ FinalProject/View/ViewController/2. Search/Player/PlayerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerViewController.swift
+//  FinalProject
+//
+//  Created by Sếp Quân on 5/8/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import UIKit
+
+final class PlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FinalProject/View/ViewController/2. Search/Player/PlayerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerViewModel.swift
+//  FinalProject
+//
+//  Created by Sếp Quân on 5/8/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import Foundation
+
+final class PlayerViewModel {
+    
+}
